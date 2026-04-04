f23ccea65b9f9d3ad822d451455080a5116d3a4a
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2026-04-04 20:18:25 -0300

    (scripts): remove building script. not needed anymore

--- scripts/build-pdf.ts
+++
@@ -1,6 +0,0 @@
-import { mdToPdf } from 'md-to-pdf';
-import { mkdir } from 'fs/promises';
-
-await mkdir('pdf', { recursive: true });
-await mdToPdf({ path: 'src/cv.md' }, { dest: 'pdf/cv.pdf', stylesheet: ['src/theme.css'] });
-console.log('PDF built at pdf/cv.pdf');
